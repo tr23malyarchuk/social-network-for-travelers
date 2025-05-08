0e92b37479c4b44835326f99a9f83c7b16e63ff0
AndriiReshetniuk <andriyreshet@gmail.com>
2025-05-08 23:31:26 +0300

    Authorization issues was fixed

---
+++ post-service/src/guards/jwt/jwt.guard.ts
@@ -0,0 +1,42 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+  Logger,
+} from '@nestjs/common';
+import axios from 'axios';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  private readonly logger = new Logger(JwtAuthGuard.name);
+  private readonly authServiceUrl = 'http://localhost:3001/authentificattion/accessVerify';
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new UnauthorizedException('No token provided');
+    }
+
+    const [bearer, token] = authHeader.split(' ');
+
+    if (bearer !== 'Bearer' || !token) {
+      throw new UnauthorizedException('Invalid token format');
+    }
+    try{
+      const response = await axios.post(`${this.authServiceUrl}`, {
+        token,
+      });
+    }catch(error)
+    {
+      throw new UnauthorizedException('Token is invalid');
+    }
+
+      // if (!response.data) {
+      //   throw new UnauthorizedException('Invalid token');
+      // }
+      return true;
+  }
+}
